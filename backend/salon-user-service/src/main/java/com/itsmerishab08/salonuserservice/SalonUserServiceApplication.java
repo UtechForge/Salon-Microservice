b3798ac0ab86c0be760a311ceb005092fe8f3e90
Utkarsh <itsmeRishab08@gmail.com>
2025-03-30 18:01:09 +0530

    user-service

---
+++ backend/salon-user-service/src/main/java/com/itsmerishab08/salonuserservice/SalonUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itsmerishab08.salonuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonUserServiceApplication.class, args);
+	}
+
+}
---
+++ backend/salon-user-service/src/test/java/com/itsmerishab08/salonuserservice/SalonUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsmerishab08.salonuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
